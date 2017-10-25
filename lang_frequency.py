67aee00e9728a306eed19731a2b70fffa3c6bdcf
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-10-25 17:50:48 +0500

    подправил названия

--- lang_frequency.py
+++ lang_frequency.py
@@ -8,19 +8,19 @@ def load_data(filepath):
         return file_handler.read()
 
 
-def get_most_frequent_words(document):
-    lower_words = document.lower()
+def get_most_frequent_words(text):
+    lower_words = text.lower()
     words = re.findall(r'\w+', lower_words)
     words_count = 10
     counted_words = Counter(words).most_common(words_count)
     return counted_words
 
 
-def counted_words_print(counted_words):
+def print_counted_words(counted_words):
     for word, quantity in counted_words:
         print('Слово "{}" повторяется {} раз'.format(word, quantity))
 
 if __name__ == '__main__':
     document = load_data(sys.argv[1])
     counted_words = get_most_frequent_words(document)
-    counted_words_print(counted_words)
+    print_counted_words(counted_words)
