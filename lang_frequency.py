fd56ae7eb659596e016bf64892f0d79f8260a1d7
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-10-25 12:01:18 +0500

    Реализовано чтение файла и поиск самых частых слов ( первые 10 )

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,17 @@
+import re
+import sys
+from collections import Counter
 
 
-def load_data(filepath):
-    pass
+def load_and_read_data(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file_handler:
+        return file_handler.read().lower()
 
 
 def get_most_frequent_words(text):
-    pass
-
+    words = re.findall(r'\w+', text)
+    print(Counter(words).most_common(10))
 
 if __name__ == '__main__':
-    pass
+    document = load_and_read_data(sys.argv[1])
+    get_most_frequent_words(document)
