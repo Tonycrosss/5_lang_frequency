1aefd42b2d7808c2c40031843154593a1b00b9c8
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-10-25 16:58:24 +0500

    Переделал переменные и название функций, разбил на составные, улучшил вывод

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,16 +3,24 @@ import sys
 from collections import Counter
 
 
-def load_and_read_data(filepath):
+def load_data(filepath):
     with open(filepath, 'r', encoding='utf-8') as file_handler:
-        return file_handler.read().lower()
+        return file_handler.read()
 
 
-def get_most_frequent_words(text):
-    words = re.findall(r'\w+', text)
+def get_most_frequent_words(document):
+    lower_words = document.lower()
+    words = re.findall(r'\w+', lower_words)
     words_count = 10
-    print(Counter(words).most_common(words_count))
+    counted_words = Counter(words).most_common(words_count)
+    return counted_words
+
+
+def counted_words_print(counted_words):
+    for word, quantity in counted_words:
+        print('Слово "{}" повторяется {} раз'.format(word, quantity))
 
 if __name__ == '__main__':
-    document = load_and_read_data(sys.argv[1])
-    get_most_frequent_words(document)
+    document = load_data(sys.argv[1])
+    counted_words = get_most_frequent_words(document)
+    counted_words_print(counted_words)
